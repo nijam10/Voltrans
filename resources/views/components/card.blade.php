e0048e4fd0fbb8bdcd64c6a3b9fefe94c135f26b
fajar288 <fajrzprayoga@gmail.com>
2025-04-28 20:03:34 +0700

    Update pages/rent & adding components/card

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,18 @@
+<div class="card card-xs bg-base-100 shadow hover:shadow-lg transition-shadow">
+    <figure class="px-4 pt-4">
+        <img src="https://placehold.co/300x200?text=Product+Image" alt="Wuling Air EV" class="rounded-xl h-48 w-full object-cover" />                        </figure>
+    <div class="card-body">
+        <h3 class="card-title">Wuling Air EV</h3>
+        <p class="text-gray-500">E-Car</p>
+            <div class="flex justify-between items-center mt-4">
+            <span class="font-bold">Rp120.000/ Day</span>
+                <div class="flex items-center">
+                    <span class="text-yellow-400">★★★★★</span>
+                    <span class="ml-1">5.0</span>
+                </div>
+            </div>
+        <div class="card-actions mt-4">
+            <button class="btn btn-soft btn-success w-full">Rent Now!</button>
+        </div>
+    </div>
+</div>

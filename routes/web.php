0d6a07b3147ea70e7631ef5de50549f50194fdbd
maulanaramadan <ramadanmaulana2110@gmail.com>
2025-04-29 15:34:25 +0700

    update detail pesanan

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,22 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RentController;
 use App\Http\Controllers\DetailController;
+<<<<<<< Updated upstream
 use Doctrine\DBAL\Schema\Index;
+=======
+use App\Http\Controllers\PesananController;
+>>>>>>> Stashed changes
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/rent', [RentController::class, 'index'])->name('rent');
 Route::get('/detail', [DetailController::class, 'index'])->name('detail');
+<<<<<<< Updated upstream
 Route::get('/detail_produk', [DetailController::class,'index'])->name('detail_produk');
+=======
+Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan');
+>>>>>>> Stashed changes
 
 
 Route::get('/welcome', function () {
